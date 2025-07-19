38d35bcb0abc383d54cb9e848144d87fb41b36ac
mirbek10 <mirbekbook@gmail.com>
2025-07-19 17:36:40 +0600

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import BuyCall from '../../components/BuyCall/BuyCall'
 import Services from '../../components/Services/Services'
 import Insurance from '../../components/insurance/Insurance'
+import Banner from '../../components/Banner/Banner'
 
 function Home() {
   return (
     <div>
+      <Banner/>
       <BuyCall/>
       <Services/>
       <Insurance/>
--- src/ui/H1.jsx
+++ src/ui/H1.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './H1.scss'
-function H1({h1}) {
+
+function H1({ h1, color, height, size, items }) {
   return (
-    <div className='h1'>
-      <h1>
-        {h1}
-      </h1>
+    <div style={{ height }} className="h1">
+      <h1
+        style={{ color, fontSize: size, textAlign: items }}
+        dangerouslySetInnerHTML={{ __html: h1 }}
+      />
     </div>
   )
 }
