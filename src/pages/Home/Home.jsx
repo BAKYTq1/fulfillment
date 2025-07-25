6bb1e513b2cca349d24ec044b216afa998b38682
Nurzada <nurzadatobokelova@gmail.com>
2025-07-26 02:15:31 +0600

    added three new block

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,31 @@ import BuyCall from '../../components/BuyCall/BuyCall'
 import Services from '../../components/Services/Services'
 import Insurance from '../../components/insurance/Insurance'
 import Banner from '../../components/Banner/Banner'
+import FulfilmentСenter from '../../components/FulfilmentСenter/FulfilmentСenter'
+import React from "react";
+import BuyCall from "../../components/BuyCall/BuyCall";
+import Services from "../../components/Services/Services";
+import Insurance from "../../components/insurance/Insurance";
+import FulfilmentСenter from "../../components/FulfilmentСenter/FulfilmentСenter";
+import Equipment from '../../components/Equipment/Equipment.jsx'
+import WorkScheme from "../../components/WorkScheme/WorkScheme";
+import { PersonalAccount } from "../../components/PersonalAccount/PersonalAccount.jsx";
 
 function Home() {
   return (
+  
+    
     <div>
       <Banner/>
-import FulfilmentСenter from '../../components/FulfilmentСenter/FulfilmentСenter'
-function Home() {
-  return (
-    <div>
-<FulfilmentСenter/>
-      <BuyCall/>
-      <Services/>
-      <Insurance/>
+      <FulfilmentСenter />
+      <BuyCall />
+      <Services />
+      <Equipment />
+      <PersonalAccount/>
+      <WorkScheme />
+      <Insurance />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
