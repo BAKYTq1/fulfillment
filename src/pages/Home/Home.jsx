3e58c8ecf6dabe40f482ed98c0a98854f4bca93d
BAKYTq1 <bsainaev@gmail.com>
2025-07-30 12:15:58 +0600

    main update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,12 +9,6 @@ import Banner from '../../components/Banner/Banner'
 // import Services from "../../components/Services/Services";
 // import Insurance from "../../components/insurance/Insurance";
 import FulfilmentСenter from '../../components/FulfilmentСenter/FulfilmentСenter'
-import Section1 from '../../components/Section1/Section1'
-import React from "react";
-import BuyCall from "../../components/BuyCall/BuyCall";
-import Services from "../../components/Services/Services";
-import Insurance from "../../components/insurance/Insurance";
-import FulfilmentСenter from "../../components/FulfilmentСenter/FulfilmentСenter";
 import Equipment from '../../components/Equipment/Equipment.jsx'
 import WorkScheme from "../../components/WorkScheme/WorkScheme";
 import { PersonalAccount } from "../../components/PersonalAccount/PersonalAccount.jsx";
